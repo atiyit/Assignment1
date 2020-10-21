970096ad6dc88dd2abceb9c96fcde267fb261aaa
atiyit <37976411+atiyit@users.noreply.github.com>
2020-10-22 00:42:24 +0200

    Assignment1

---
+++ Models/MessageModel.cs
@@ -0,0 +1,25 @@
+﻿using MessageComponent;
+
+namespace Assignment.Models
+{
+    public class MessageModel
+    {
+
+        public static MessageModel messageModel;
+        private MessageModel()
+        {}
+
+        public static MessageModel getInstance()
+        {
+            if (messageModel == null)
+            {
+                messageModel = new MessageModel();
+            }
+
+            return messageModel;
+        }
+
+        public string messageManage { get; set; }
+        
+    }
+}
